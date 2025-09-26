74054737aa1fa4cd6b9f428f734b365b5cee24f9
ftljk <2510317228@qq.com>
2025-09-26 18:18:43 +0800

    refractor equals for BinaryStringType

--- geaflow/geaflow-common/src/main/java/org/apache/geaflow/common/type/primitive/BinaryStringType.java
+++ geaflow/geaflow-common/src/main/java/org/apache/geaflow/common/type/primitive/BinaryStringType.java
@@ -42,6 +42,23 @@ public class BinaryStringType implements IType<BinaryString> {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
     public String getName() {
         return Types.TYPE_NAME_BINARY_STRING;
     }
