a593ebe6bea9e926cb64acf976a80d0d569a5c9f
huaiyu <hongzhihao.hzh@antgroup.com>
2025-11-14 10:35:17 +0800

    update license info

--- geaflow/geaflow-dsl/geaflow-dsl-plan/src/main/java/org/apache/geaflow/dsl/udf/graph/JaccardSimilarityAlgorithm.java
+++ geaflow/geaflow-dsl/geaflow-dsl-plan/src/main/java/org/apache/geaflow/dsl/udf/graph/JaccardSimilarityAlgorithm.java
@@ -1,6 +1,20 @@
 /*
- * Ant Group
- * Copyright (c) 2004-2025 All Rights Reserved.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.geaflow.dsl.udf.graph;
 
@@ -67,7 +81,7 @@ public class JaccardSimilarityAlgorithm implements AlgorithmUserFunction<Object,
             if (vertices.f0.equals(vertexId)) {
                 List<RowEdge> edges = context.loadEdges(EdgeDirection.BOTH);
                 long edgeCount = edges.size();
-                // 将f0的相邻节点数发给f1
+                // send neighbour count of A to B
                 context.sendMessage(vertices.f1, PREFIX + edgeCount);
             }
 
